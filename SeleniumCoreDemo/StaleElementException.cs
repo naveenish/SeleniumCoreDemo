bcd929882dab44e372c054a3665c2aa0759580f6
navee <navee@LAPTOP-GG9LB8TA>
2021-10-24 18:38:19 +1300

    Add project files.

---
+++ SeleniumCoreDemo/StaleElementException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SeleniumCoreDemo
+{
+    [Serializable]
+    internal class StaleElementException : Exception
+    {
+        public StaleElementException()
+        {
+        }
+
+        public StaleElementException(string message) : base(message)
+        {
+        }
+
+        public StaleElementException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StaleElementException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
